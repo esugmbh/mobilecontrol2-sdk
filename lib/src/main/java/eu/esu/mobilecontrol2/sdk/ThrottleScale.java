1cb68c97ee7d7611ab9efedc9cfac81f658c2006
Christian Held <christian.held@esu.eu>
2015-03-09 13:52:39 +0100

    Updated license header.

--- lib/src/main/java/eu/esu/mobilecontrol2/sdk/ThrottleScale.java
+++ lib/src/main/java/eu/esu/mobilecontrol2/sdk/ThrottleScale.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2015 ESU electronic solutions ulm GmbH & Co KG
  *
  * This software may be modified and distributed under the terms
- * of the MIT license. See the LICENSE.txt file for details.
+ * of the MIT license. See the LICENSE file for details.
  */
 
 package eu.esu.mobilecontrol2.sdk;
